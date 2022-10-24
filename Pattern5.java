55e6b36f809ee3c009c1a76f71cfe20af25cf618
Ramkumar <115568440+RamkumarAnnadurai@users.noreply.github.com>
2022-10-24 16:29:54 +0530

    Fifth Program

---
+++ Pattern5.java
@@ -0,0 +1,15 @@
+class Test {   
+    public static void main(String[] args){  
+        int i,j,n=15; 
+        for(i=0;i<n;System.out.println(),i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==0||i==n-1||i+j<=n/2||i-j>=n/2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+        }
+    }
+}
