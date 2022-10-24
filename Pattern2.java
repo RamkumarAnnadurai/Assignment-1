ca42acce120aff38a72800d0fb4fc746adfe0b4f
Ramkumar <115568440+RamkumarAnnadurai@users.noreply.github.com>
2022-10-24 16:27:43 +0530

    Second Program

---
+++ Pattern2.java
@@ -0,0 +1,12 @@
+class Test {   
+    public static void main(String[] args){  
+        int i,j,n=4; 
+        for(i=0;i<n;System.out.println(),i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                System.out.print(i+1+" ");   
+            }
+        }
+    }
+}
